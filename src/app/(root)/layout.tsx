b0598e8779c7ff0630a973ec7ef85abdebf14f02
Manh Nguyen <danny.nguyen.2702@gmail.com>
2025-05-08 06:50:30 +0700

    Left Sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,19 @@
 import Navbar from "@/components/navigation/navbar";
+import LeftSideBar from "@/components/navigation/navbar/LeftSideBar";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSideBar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max:md:pb-14 sm:px-14">
+          {children}
+        </section>
+      </div>
     </main>
   );
 };
