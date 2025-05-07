ea7116a5e62d0624a387460a72353bce7068124b
Manh Nguyen <danny.nguyen.2702@gmail.com>
2025-05-07 07:14:42 +0700

    Creating Auth Form for users to login with username and password

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "", name: "", username: "" }}
+      onsubmit={(data) =>
+        Promise.resolve({
+          success: true,
+          data,
+        })
+      }
+    />
+  );
 };
 
 export default SignUp;
